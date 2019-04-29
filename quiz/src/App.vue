8b3816a3010af800be7318633dd2cca6a75e9a3d
David Harris <david.cl.harris@gmail.com>
2019-04-29 12:29:58 -0400

    Added Bootstrap Vue

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <Header />
-    <QuestionBox />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import QuestionBox from './components/QuestionBox.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    Header,
-    QuestionBox
+    HelloWorld
   }
 }
 </script>
