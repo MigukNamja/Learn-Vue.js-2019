4b0f5c4f39182d5acfdfadec442fbec6eaff81d4
David Harris <david.cl.harris@gmail.com>
2019-05-01 20:38:44 -0400

    Part 3.2

--- adopt-pets/src/views/Cats.vue
+++ adopt-pets/src/views/Cats.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    Cats
+    <h1>Cats for Adoption</h1>
+    <b-table
+      striped
+      hover
+      head-variant='dark'
+      :items="cats"
+    >
+      <template slot="name" slot-scope="data">
+        <router-link :to="`/pets/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
   </div>
 </template>
+
+<script>
+// The @ symbol is a shortcut to/for the 'src' directory
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+}
+</script>
--- adopt-pets/src/views/Dogs.vue
+++ adopt-pets/src/views/Dogs.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    Dogs
+    <h1>Dogs for Adoption</h1>
+    <b-table striped hover :items="dogs"></b-table>
   </div>
 </template>
+
+<script>
+// The @ symbol is a shortcut to/for the 'src' directory
+import dogs from '@/data/dogs'
+
+export default {
+  data () {
+    return {
+      dogs
+    }
+  }
+}
+</script>
