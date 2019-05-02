1dfadf8cfc5a4d4535bcebb454ddfe30982a10e3
MigukNamja <MigukNamja@gmail.com>
2019-05-02 19:24:28 -0400

    Finished with the display part, about to start adding pets

--- adopt-pets/src/views/Dogs.vue
+++ adopt-pets/src/views/Dogs.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
     <h1>Dogs for Adoption</h1>
-    <b-table striped hover :items="dogs"></b-table>
+    <b-table
+      striped
+      hover
+      head-variant='dark'
+      :items="dogs"
+      >
+        <template slot="name" slot-scope="data">
+          <router-link :to="`/pets/dogs/${data.index}`">
+            {{ data.value }}
+          </router-link>
+        </template>
+    </b-table>
   </div>
 </template>
 
 <script>
-// The @ symbol is a shortcut to/for the 'src' directory
-import dogs from '@/data/dogs'
+import { mapState } from 'vuex'
 
 export default {
   data () {
-    return {
-      dogs
-    }
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
   }
 }
 </script>
